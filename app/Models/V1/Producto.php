4369dff8d1b00d390037897d6a45c253440354e6
Kiba5402 <cristian5402@gmail.com>
2022-04-22 23:07:13 -0500

    Login pt1

---
+++ app/Models/V1/Producto.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\V1;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Producto extends Model
+{
+    use HasFactory;
+    use Sluggable;
+    use SluggableScopeHelpers;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
+}
---
+++ database/seeders/User/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders\User;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'     => 'Administrador',
+            'email'    => 'admin@evertec.com',
+            'password' => Hash::make('evertec2022*'),
+        ]);
+
+        User::create([
+            'name'     => 'Usuario 1',
+            'email'    => 'user_1@evertec.com',
+            'password' => Hash::make('evertec2022*'),
+        ]);
+
+        User::create([
+            'name'     => 'Usuario 2',
+            'email'    => 'user_2@evertec.com',
+            'password' => Hash::make('evertec2022*'),
+        ]);
+
+        User::create([
+            'name'     => 'Usuario 3',
+            'email'    => 'user_3@evertec.com',
+            'password' => Hash::make('evertec2022*'),
+        ]);
+
+        User::create([
+            'name'     => 'Usuario 4',
+            'email'    => 'user_4@evertec.com',
+            'password' => Hash::make('evertec2022*'),
+        ]);
+    }
+}
